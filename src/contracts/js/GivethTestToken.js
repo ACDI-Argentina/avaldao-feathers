0c60728f233a6e84ffeefdf3bff2c642f559722a
Mauricio Coronel <cmauriciocoronel@gmail.com>
2021-07-13 20:56:38 -0300

    Versión inicial (Give4Forest Feathers commit d780725f)

---
+++ src/contracts/js/GivethTestToken.js
@@ -0,0 +1,5 @@
+const generateClass = require('eth-contract-class').default;
+
+const GivethTestTokenArtifact = require('../build/contracts/GivethTestToken.json');
+
+module.exports = generateClass(GivethTestTokenArtifact.abi, GivethTestTokenArtifact.bytecode);
