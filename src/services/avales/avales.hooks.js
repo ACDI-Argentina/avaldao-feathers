74b381988e07c388fe50e98a60454d33865beb78
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-01-04 21:16:24 -0300

    Add hook to check address
    Issue #10

--- src/services/avales/avales.hooks.js
+++ src/services/avales/avales.hooks.js
@@ -1,9 +1,41 @@
+
+
+const log = () => context => {
+  console.log(context.data);
+  const userId = context.params.payload.userId;
+  console.log(`userId:`,userId);
+
+  return context;
+};
+
+
+const onlyRole = (role) => context => {
+  let userId;
+
+  if(context.params.payload && context.params.payload.userId){
+    userId = context.params.payload.userId;
+  }
+
+  if(!userId){
+    throw new Error('Unauthorized');  
+  }
+  const addressData = context.data[`${role}Address`];
+  
+  if(addressData !== userId){
+    throw new Error(`Unauthorized. User: [${userId}] is not allowed to execute current action`);
+  }
+  
+  return context;
+};
+
+
+
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [onlyRole("solicitante")],
     update: [],
     patch: [],
     remove: []
