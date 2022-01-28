5ed909ea9b4a9fc0961af6696553cb24822feaa0
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-01-28 19:01:20 -0300

    Remove deprecated files

--- src/services/authentication/authentication.js
+++ src/services/authentication/authentication.js
@@ -9,7 +9,7 @@ module.exports = function init() {
   // Set up authentication with the secret
   app.configure(authentication(config));
   app.configure(jwt());
-  app.configure(web3());
+  //app.configure(web3());
 
   // The `authentication` service is used to create a JWT.
   // The before `create` hook registers strategies that can be used
