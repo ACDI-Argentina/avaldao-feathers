f0dad40830e960988c9a68f47208bb2bdf0e4ecc
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-01-28 18:50:03 -0300

    Remove unused hooks

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,39 +1,10 @@
 const commons = require('feathers-hooks-common');
-const { restrictToOwner } = require('feathers-authentication-hooks');
-const { toChecksumAddress } = require('web3-utils');
-
-const notifyOfChange = require('../../hooks/notifyOfChange');
-const sanitizeAddress = require('../../hooks/sanitizeAddress');
-const setAddress = require('../../hooks/setAddress');
-const resolveFiles = require('../../hooks/resolveFiles');
-
-const normalizeId = () => context => {
-  if (context.id) {
-    context.id = toChecksumAddress(context.id);
-  }
-  return context;
-};
-
-const restrict = [
-  normalizeId(),
-  restrictToOwner({
-    idField: 'address',
-    ownerField: 'address',
-  }),
-];
-
-const address = [
-  setAddress('address'),
-  sanitizeAddress('address', { required: true, validate: true }),
-];
-
-// TODO write a hook to prevent overwriting a non-zero giverId with 0
 
 module.exports = {
   before: {
     all: [],
-    find: [/*sanitizeAddress('address')*/],
-    get: [/*normalizeId(),*/commons.discardQuery('$disableStashBefore')],
+    find: [],
+    get: [commons.discardQuery('$disableStashBefore')],
     create: [],
     update: [],
     patch: [],
