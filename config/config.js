3371dab5cb2f67ac0f8bb4c572c0589a6d0f2b34
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-02-08 09:19:34 -0300

    Issue #42. Preparar ambiente de produccion
    Se actualiza config para que utilice el archivo .env del directorio raiz

--- config/config.js
+++ config/config.js
@@ -1,8 +1,4 @@
-const path = require('path');
-const dotenv = require('dotenv');
-
-const configPath = path.resolve(__dirname, `./.env`)
-dotenv.config({ path: configPath });
+require('dotenv').config();
 
 module.exports = {
   mongodb: process.env.MONGO_DB,
