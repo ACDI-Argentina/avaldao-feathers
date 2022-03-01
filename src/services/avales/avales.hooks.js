56f49128e5d8fcf39c5abb025067ad7e2f6f5f45
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-03-01 13:53:50 -0300

    Issue #51. Editar aval
    Se agregan nuevos hooks para la actualizacion de un aval

--- src/services/avales/avales.hooks.js
+++ src/services/avales/avales.hooks.js
@@ -8,7 +8,6 @@ const log = () => context => {
   return context;
 };
 
-
 const onlyAuthenticated = () => context => {
   try{
     const userId = context.params.payload.userId; 
@@ -23,7 +22,7 @@ const onlyAuthenticated = () => context => {
 
 
 //Comprueba que el address del solicitante sea el mismo que llega en el body
-const checkAvalSolicitante = () => context => {
+const onlySolicitante = () => context => {
   let userId;
   try {
     userId = context.params.payload.userId;
@@ -41,6 +40,54 @@ const checkAvalSolicitante = () => context => {
 
   return context;
 };
+
+//Comprueba que el usuario quien solicito originalmente el aval
+const onlySolicitanteOriginal = () => async context => { 
+  let userId;
+  try {
+    userId = context.params.payload.userId;
+  } catch (err) {
+    //property doesnt exist 
+  }
+
+  if (!userId) {
+    throw new UnauthorizedError(); //TODO: Add details
+  }
+  
+  const storedAval = await context.service.get(context.id);
+
+  if(storedAval.solicitanteAddress !== userId){
+    throw new ForbiddenError();
+  }
+
+
+  return context;
+};
+
+const onlyAvalInStatus = status => async context => {
+  const storedAval = await context.service.get(context.id);
+  if(!storedAval){
+    throw new Error("Not Found");
+  }
+
+  if(storedAval.status !== status){
+    throw new Error("Incorrect status");
+  }
+
+  return context;
+}
+
+
+//Mantiene el estado previo de un aval, evita que al realizarse una actualizacion se modifique el estado
+const keepStatus = () => async context => {
+  const storedAval = await context.service.get(context.id);
+  if(!storedAval){
+    throw new Error("Not Found");
+  }
+  context.data.status = storedAval.status;
+  
+  return context;
+}
 
 
 //'AVALDAO_ROLE','SOLICITANTE_ROLE','COMERCIANTE_ROLE','AVALADO_ROLE'
@@ -101,11 +148,20 @@ module.exports = {
     create: [
       onlyAuthenticated(),
       log(),
-      checkAvalSolicitante(),
+      onlySolicitante(),
       onlyJWTRole("SOLICITANTE_ROLE")
     ], 
-    update: [],
-    patch: [onlyAuthenticated(),onlyAvaldaoCanAccept()],
+    update: [
+      onlyAvalInStatus(0),
+      onlyAuthenticated(),
+      onlySolicitanteOriginal(),
+      onlySolicitante(),
+      keepStatus()
+    ], 
+    patch: [
+      onlyAuthenticated(),
+      onlyAvaldaoCanAccept()
+    ],
     remove: []
   },
 
